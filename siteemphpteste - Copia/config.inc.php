ae90b46b8910d2a48ae8c891aaf1afc157eb9804
Kayo victor <kayovictorvilar@gmail.com>
2024-11-27 21:38:05 -0300

    Delete siteemphpteste - Copia directory

--- siteemphpteste - Copia/config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-  // 1ª Etapa - Conexão com Servidor 
-  $conexao = mysqli_connect("localhost","kayo","");
-  // 2ª Etapa - Selecionar Banco de dados
-  $bd = mysqli_select_db($conexao,"bd1");
--- siteemphpteste - Copia/templates/menu.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <a href="?pg=conteudo"> Página Inicial </a> |
-    <a href="?pg=quemsomos">Quem Somos </a> |
-    <a href="?pg=contato">Fale Conosco </a> |
-    <a href="?pg=login">Login</a> |
-    <a href="?pg=cadastro">Cadastro</a>
-</nav>
